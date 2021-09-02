2876a6f300b5fbb602cb9c1b52da45b52f02b1a3
Gustavo Barros <gustavo.pereira@ihm.com.br>
2021-09-01 22:57:30 -0300

    Front end base files changes

--- mercy-frontend/src/app/app-routing.module.ts
+++ mercy-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { PatientsComponent } from './components/patients/patients.component';
+import { SystemLayoutComponent } from './components/system-layout/system-layout.component';
+import { AuthGuard } from './services/auth/auth.guard';
+import { Views } from './util/views.enum';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SystemLayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: Views.patients.url,
+        component: PatientsComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  providers: [AuthGuard],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- mercy-frontend/src/app/app.module.ts
+++ mercy-frontend/src/app/app.module.ts
@@ -1,18 +1,78 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorIntl, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule, MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialFileInputModule } from 'ngx-material-file-input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PatientDialogComponent } from './components/patient-dialog/patient-dialog.component';
+import { PatientsComponent } from './components/patients/patients.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { SystemLayoutComponent } from './components/system-layout/system-layout.component';
+import { AuthInterceptor } from './services/auth/auth.interceptor';
+import { AuthService } from './services/views/auth.service';
+import { PatientService } from './services/views/patient.service';
+import { getPtPaginatorIntl } from './util/pt-paginator-intl';
+import { LandingRoutingModule } from './views/landing/landing-routing.module';
+import { LandingComponent } from './views/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    SystemLayoutComponent,
+    PatientsComponent,
+    HeaderComponent,
+    PatientDialogComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    LandingRoutingModule,
+    AppRoutingModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDividerModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatCardModule,
+    MatIconModule,
+    MatSelectModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MaterialFileInputModule,
+    MatSnackBarModule,
+    MatExpansionModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: MatPaginatorIntl, useValue: getPtPaginatorIntl()},
+    { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {duration: 5000}},
+    AuthService,
+    PatientService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
